2af45938fea44ac93a6c9ff8be46fe7605a00780
Harry Lum <hlum1@stuy.edu>
2016-10-07 12:14:39 -0400

    wow

--- donow.py
+++ donow.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 import hashlib
 
 app = Flask(__name__)
+Session
 
 @app.route('/')
 def hello():
@@ -20,23 +21,28 @@ def authenticate():
 		d = line.split(',')
 		info[d[0]] = d[1].strip("\n")
 	
-	if (request.form['auth'] == "login"):
-		password = hashlib.sha1(request.form['pass']).hexdigest()
-		print info[request.form['user']] + "\n"
-		print password + "\n"
+	user = request.form['user'].lower()
+	password = hashlib.sha1(request.form['pass']).hexdigest()
 		
-		if (request.form['user'].lower() in info and info[request.form['user']] == password):
-				return render_template("template2.html",match="are valid.")
+	
+	if (request.form['auth'] == "login"):
+		
+		
+		if (user in info):
+			if (info[user] == password):
+				return render_template("template2.html",match="You have logged in!")
+			else:
+				return render_template("template2.html",match="Your password is wrong.")
 		else:
-				return render_template("template2.html",match="are invalid." + info[request.form['user']] == password)
-
+			return render_template("template2.html",match="Your username is wrong.")
+			
 	else:
-		if (request.form['user'].lower() not in info):
+		if (user not in info):
 			x = open("data/info.txt",'a')
-			x.write(request.form['user'].lower() + "," + hashlib.sha1(request.form['pass']).hexdigest() + "\n")
-			return render_template("template2.html", match="have been registered")
+			x.write(user + "," + password + "\n")
+			return render_template("template2.html", match="You have registered!")
 		else:
-			return render_temlate("template2.html", match="have been taken")
+			return render_template("template2.html", match="Your username has been taken.")
 			
 if __name__ == "__main__":
 	app.debug = True
